a0e09b7645d161014248b4ee6b9b8c1995b74f40
glaserg0001 <glaserg001@gmail.com>
2021-10-12 01:04:54 +0300

    nav

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,26 @@
-console.log('Hello, World')
+class toggleNavigation {
+    constructor(container, btn) {
+        this.btn = btn;
+        this.container = container;
+        this.containerOpened = 'm-opened';
+        this.btnActive = 'm-active';
+        this.btnPrimary = '.js-primary';
+    }
+
+    init() {
+        const btn = document.querySelectorAll(this.btn);
+        const container = document.querySelector(this.container);
+        const btnPimary = document.querySelector(this.btnPrimary);
+
+        btn.forEach(i => {
+            i.addEventListener('click', () => {
+                container.classList.toggle(this.containerOpened);
+                btnPimary.classList.toggle(this.btnActive);
+            })
+        })
+    }
+}
+
+let mainNav = new toggleNavigation('.js-nav', '.js-nav-btn')
+
+mainNav.init();
