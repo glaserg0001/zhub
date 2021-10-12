38db3f9ad577b7921060ac074219f4b8360562fb
glaserg0001 <glaserg001@gmail.com>
2021-10-13 00:53:11 +0300

    navbar

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,30 @@
 class toggleNavigation {
-    constructor(container, btn) {
+    constructor(name, btn) {
+        this.root = 'html'
+        this.openedClass = `${name}--opened`;
         this.btn = btn;
-        this.container = container;
-        this.containerOpened = 'm-opened';
-        this.btnActive = 'm-active';
-        this.btnPrimary = '.js-primary';
+        // this.container = container;
+        // this.containerOpened = 'm-opened';
+        // this.btnActive = 'm-active';
+        // this.btnPrimary = '.js-primary';
     }
 
     init() {
         const btn = document.querySelectorAll(this.btn);
-        const container = document.querySelector(this.container);
-        const btnPimary = document.querySelector(this.btnPrimary);
+        // const container = document.querySelector(this.container);
+        // const btnPrimary = document.querySelector(this.btnPrimary);
+        const root = document.querySelector(this.root);
 
         btn.forEach(i => {
             i.addEventListener('click', () => {
-                container.classList.toggle(this.containerOpened);
-                btnPimary.classList.toggle(this.btnActive);
+                // container.classList.toggle(this.containerOpened);
+                // btnPrimary.classList.toggle(this.btnActive);
+                root.classList.toggle(this.openedClass)
             })
         })
     }
 }
 
-let mainNav = new toggleNavigation('.js-nav', '.js-nav-btn')
+let mainNav = new toggleNavigation('header-navbar', '.js-nav-btn')
 
 mainNav.init();
