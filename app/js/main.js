07c374152ac10af6abbe3026724307e146126021
glaserg0001 <glaserg001@gmail.com>
2021-10-25 00:20:12 +0300

    filter & seo

--- app/js/main.js
+++ app/js/main.js
@@ -1,48 +1,55 @@
-class toggleNavigation {
+class NavigationBar {
     constructor(name, btn) {
         this.root = 'body'
-        this.openedClass = `${name}__opened`;
+        this.openedClass = `${name}--opened`;
         this.btn = btn;
     }
 
     init() {
+        this.toggleSidebar();
+    }
+
+    toggleSidebar() {
         const btn = document.querySelectorAll(this.btn);
-        const root = document.querySelector(this.root);
 
         btn.forEach(i => {
-            i.addEventListener('click', () => {
-                let position = window.pageYOffset;
-                
-                if (position) {
-                    root.style.top = `-${position}px`;
-                    this.scrollposition = position;
-                }
-                
-                root.classList.toggle(this.openedClass);
-
-                if (!root.classList.contains(this.openedClass)) {
-                    parseInt(root.style.top) && (root.style.top = null);
-                    window.scrollTo(0, this.scrollposition);
-                    this.scrollposition = 0;
-                }
-            })
+            i.addEventListener('click', this.toggleEvent.bind(this))
         })
+    }
+
+    toggleEvent() {
+        const root = document.querySelector(this.root);
+        console.log(this.root)
+        let position = window.pageYOffset;
+
+        if (position) {
+            root.style.top = `-${position}px`;
+            this.scrollposition = position;
+        }
+        
+        root.classList.toggle(this.openedClass);
+
+        if (!root.classList.contains(this.openedClass)) {
+            parseInt(root.style.top) && (root.style.top = null);
+            window.scrollTo(0, this.scrollposition);
+            this.scrollposition = 0;
+        }
     }
 }
 
-let mainNav = new toggleNavigation('header-navbar', '.js-nav-btn')
+new NavigationBar('header-navbar', '.js-nav-btn').init();
+new NavigationBar('filter-navbar', '.js-filter-btn').init();
 
-mainNav.init();
+
 
 
 // select2
 $('.js-select').select2({
+    width: 'style',
     // placeholder: 'Оберіть категорію',
     // allowClear: true,
     // theme: "secondary",
-    width: 'style',
     // selectionCssClass: 'select2-selection--small',
     // dropdownCssClass: 'select2-dropdown--small',
-    // minimumResultsForSearch: -1,
-    // multiple: false
+    // minimumResultsForSearch: -1
 });
