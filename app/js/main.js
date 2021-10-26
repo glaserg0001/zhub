d63760c62273be9f62507bdd1239f82fd8bbae53
glaserg0001 <glaserg001@gmail.com>
2021-10-26 19:47:47 +0300

    product tile and search

--- app/js/main.js
+++ app/js/main.js
@@ -79,3 +79,26 @@ $('.js-select').select2({
     // dropdownCssClass: 'select2-dropdown--small',
     // minimumResultsForSearch: -1
 });
+
+// https://select2.org/dropdown
+// https://select2.org/selections
+function formatState (state) {
+    if (!state.id) {
+      return state.text;
+    }
+  
+    var baseUrl = "/user/pages/images/flags";
+    var $state = $(
+      '<span><img class="img-flag" /> <span></span></span>'
+    );
+  
+    // Use .text() instead of HTML string concatenation to avoid script injection issues
+    $state.find("span").text(state.text);
+    $state.find("img").attr("src", baseUrl + "/" + state.element.value.toLowerCase() + ".png");
+  
+    return $state;
+  };
+  
+  $(".js-example-templating").select2({
+    templateSelection: formatState
+  });
